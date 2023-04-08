66e4235038d730be61134bc875d50e9dda828cf4
Islam Alybaev <117467614+kermitlafrog61@users.noreply.github.com>
2023-04-09 05:08:02 +0600

    updated statics

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -30,4 +29,4 @@ urlpatterns = [
     path('', include('apps.products.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
