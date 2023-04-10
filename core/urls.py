8f6f5541015ebaa09cf042bd66d5f108ab7604c3
kermitlafrog61 <islamchikalybaev@gmail.com>
2023-04-10 16:45:06 +0600

    media for render

--- core/urls.py
+++ core/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('', include('apps.products.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
